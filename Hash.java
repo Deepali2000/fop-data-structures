322b812c261a45a54bd40ed59b7bfdb3eba04319
Deepali <deepalijindal04@gmail.com>
2019-11-23 17:47:05 +0530

    largestnum

--- Hash.java
+++ Hash.java
@@ -1,7 +1,6 @@
 import java.util.*;
 class Hash { 
-
-    static boolean isSubset(int arr1[],int arr2[], int k, int n) 
+   static boolean isSubset(int arr1[],int arr2[], int k, int n) 
     { 
         int i = 0; 
         int j = 0; 
@@ -22,14 +21,12 @@ class Hash {
         int arr1[] = {11, 1, 13, 21, 3, 7}; 
         int arr2[] = {11, 3, 7, 1}; 
           
-        int m = arr1.length; 
+        int k = arr1.length; 
         int n = arr2.length; 
       
         if(isSubset(arr1, arr2, m, n)) 
-            System.out.print("arr2[] is "
-                  + "subset of arr1[] "); 
+            System.out.print("arr2[] is "+ "subset of arr1[] "); 
         else
-            System.out.print("arr2[] is "
-             + "not a subset of arr1[]");  
+            System.out.print("arr2[] is "+ "not a subset of arr1[]");  
     } 
 } 
