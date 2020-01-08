7fe36225808cce0df19c66dd8301d4ae396bdfb8
Deepali <deepalijindal04@gmail.com>
2020-01-08 09:31:27 +0530

    missing number

--- EVERY day pratcie/missingnumber.java
+++ EVERY day pratcie/missingnumber.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.*;
 class missingnumber
 { 
@@ -21,28 +20,5 @@ class missingnumber
         int[] arr = { 1, 2, 3, 5 }; 
         System.out.println(getMissingNo(arr, arr.length)); 
     } 
-=======
-import java.util.*;
-class missingnumber
-{ 
-   // a represents the array 
-    // n : Number of elements in array a 
-    static int getMissingNo(int p[], int q)  
-    { 
-        int total = 1; 
-        for (int i = 2; i <= (q + 1); i++) 
-        { 
-            total += i; 
-            total -= p[i - 2]; 
-        } 
-        return total; 
-    } 
-  
-    // Driver Code 
-    public static void main(String[] args) 
-    { 
-        int[] arr = { 1, 2, 3, 5 }; 
-        System.out.println(getMissingNo(arr, arr.length)); 
-    } 
->>>>>>> c1cdf7413abff4047bb6a94509a98fb1846eed32
+
 } 
