ed9f0c01267672b810957f90a3b420f239792bdb
Deepali <deepalijindal04@gmail.com>
2020-01-22 22:27:34 +0530

    Add files via upload

---
+++ String/reverse.java
@@ -0,0 +1,14 @@
+        public class reverse{
+
+public static void main(String args[])
+{
+     String s="I love India";
+     String str[]=s.split(" ");
+     for(int i=str.length-1;i>=0;i--)
+     {
+         System.out.print(str[i]+" ");
+     }
+}
+  
+}
+
